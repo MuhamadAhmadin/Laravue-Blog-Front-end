4df8f80cf1052727e5d364935871ae86b682f90a
Muhamad Ahmadin <ahmadinations@gmail.com>
2021-11-06 05:18:21 +0700

    revamp style

--- src/views/post/index.vue
+++ src/views/post/index.vue
@@ -1,31 +1,40 @@
 <template>
   <!-- Begin page content -->
-  <main class="flex-shrink-0">
+  <main class="flex-shrink-0 mt-5">
     <div class="container">
-      <h1 class="mt-5">Index Post</h1>
-      <div class="row">
-        <div class="col-md-12">
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Title</th>
-                <th scope="col">Content</th>
-                <th scope="col">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(post, index) in posts" :key="index">
-                <th scope="row">{{ post.id }}</th>
-                <td>{{ post.title }}</td>
-                <td>{{ post.content }}</td>
-                <td>
-                    <router-link :to="{name: 'post.edit', params:{id: post.id }}" class="btn btn-warning">Edit</router-link>
-                    <button class="btn btn-danger ms-2">Delete</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+      <div class="card">
+        <h5 class="card-header">POST LIST</h5>
+        <div class="card-body">
+          <button type="button" class="btn btn-sm btn-success">ADD POST</button>
+          <div class="row mt-3">
+            <div class="col-md-12">
+              <table class="table table-hover">
+                <thead class="table-dark">
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">TITLE</th>
+                    <th scope="col">CONTENT</th>
+                    <th scope="col">ACTION</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(post, index) in posts" :key="index">
+                    <th scope="row">{{ post.id }}</th>
+                    <td>{{ post.title }}</td>
+                    <td>{{ post.content }}</td>
+                    <td>
+                      <router-link
+                        :to="{ name: 'post.edit', params: { id: post.id } }"
+                        class="btn btn-sm btn-warning"
+                        >Edit</router-link
+                      >
+                      <button class="btn btn-sm btn-danger ms-2">Delete</button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -33,28 +42,31 @@
 </template>
 
 <script>
-import axios from 'axios'
-import { onMounted, ref } from 'vue'
+import axios from "axios";
+import { onMounted, ref } from "vue";
 export default {
-    setup() {
-        // reactive state
-        let posts = ref([])
+  setup() {
+    // reactive state
+    let posts = ref([]);
 
-        // mounted
-        onMounted(() => {
-            // get api from laravel backend
-            axios.get('http://localhost/backend-api/public/api/post').then(response => {
-                // assign state posts with response data
-                posts.value = response.data.data
-                console.log(response.data)
-            }).catch(error => {
-                console.log(error.response.data)
-            })
+    // mounted
+    onMounted(() => {
+      // get api from laravel backend
+      axios
+        .get("http://localhost/backend-api/public/api/post")
+        .then((response) => {
+          // assign state posts with response data
+          posts.value = response.data.data;
+          console.log(response.data);
         })
+        .catch((error) => {
+          console.log(error.response.data);
+        });
+    });
 
-        return {
-            posts
-        }
-    }
+    return {
+      posts,
+    };
+  },
 };
 </script>
