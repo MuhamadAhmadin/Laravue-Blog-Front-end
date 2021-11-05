bcc99c9ef6d18ac3e160ba34563cb3b934d0bc24
Muhamad Ahmadin <ahmadinations@gmail.com>
2021-11-06 04:44:30 +0700

    bootstrap 5 require poperjs/core, so read it through stackoverflow about it

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+// import vue router
+import { createRouter, createWebHistory } from "vue-router";
+
+// define routes
+const routes = [
+    {
+        path: '/',
+        name: 'post.index',
+        component: () => import('@/views/post/index.vue')
+    },
+    {
+        path: '/create',
+        name: 'post.create',
+        component: () => import('@/views/post/create.vue')
+    },
+    {
+        path: '/edit/:id',
+        name: 'post.edit',
+        component: () => import('@views/post/edit.vue')
+    }
+];
+
+// create router
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router;
