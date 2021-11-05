a2453cec158454f8decc2a11219a7310aa681a87
Muhamad Ahmadin <ahmadinations@gmail.com>
2021-11-06 04:34:15 +0700

    base view for post

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Import bootstrap, Popper and jQuery
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery/dist/jquery.min'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min'
+
 createApp(App).mount('#app')
